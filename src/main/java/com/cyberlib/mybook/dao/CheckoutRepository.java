79079910e5f79fde90c7b6ebac4943fd231a84aa
hb-17 <softdevhasan@gmail.com>
2024-07-24 16:04:52 +0700

    chekcout entity and repo

---
+++ src/main/java/com/cyberlib/mybook/dao/CheckoutRepository.java
@@ -0,0 +1,10 @@
+package com.cyberlib.mybook.dao;
+
+
+import com.cyberlib.mybook.entity.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+    Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+}
+
